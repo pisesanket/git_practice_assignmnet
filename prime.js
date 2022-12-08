9047fdf9e83fa5b17b8bda5cfb0f52dbd0bd9d40
pisesanket <85410203+pisesanket@users.noreply.github.com>
2022-12-08 21:12:37 +0530

    Update prime.js

--- prime.js
+++ prime.js
@@ -1,27 +1,13 @@
 //to check number is prime or not
 
-<<<<<<< m2_code_fix
+
 function isPrime(num) {
   let count = 0;
   for (let i = 1; i <= num; i++) {
     if (num % i === 0) {
       //on this line you will have to make changes.
       count++;
-=======
-function isPrime(num){
-    let count=0;
-    for(let i=1;i<=Math.sqrt(num);i++){
-        if(num%i==0) //made the changes.
-        {
-            count++;
-        }
-    }
-    if(count==1){
-        console.log("yes");
-    }
-    else{
-        console.log("no");
->>>>>>> main
+
     }
   }
   if (count === 2) {
