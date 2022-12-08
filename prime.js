c17e4456d1b41995dfac63a6a77ec182ac406067
abhay <itsyourabhay@gmail.com>
2022-12-08 20:45:17 +0530

    I fixed the code

--- prime.js
+++ prime.js
@@ -1,19 +1,18 @@
 //to check number is prime or not
 
-function isPrime(num){
-    let count=0;
-    for(let i=1;i<=Math.sqrt(num);i++){
-        if(num%i==1) //on this line you will have to make changes.
-        {
-            count++;
-        }
+function isPrime(num) {
+  let count = 0;
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      //on this line you will have to make changes.
+      count++;
     }
-    if(count==1){
-        console.log("yes");
-    }
-    else{
-        console.log("no");
-    }
+  }
+  if (count === 2) {
+    console.log("Yes");
+  } else {
+    console.log("No");
+  }
 }
 
-isPrime(17);
+isPrime(88);
